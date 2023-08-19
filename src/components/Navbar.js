ce32aa466e35dea76c3d54b8c19dd996366bf40b
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-08-19 22:02:21 +0530

    Done

---
+++ src/components/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
+
+
+
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>C</span>oding
+            <span>C</span>lub
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Tutorials</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">Session</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Notes</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">contact</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* 3rd social media links */}
+        <div className="social-media">
+          <ul className="social-media-desktop">
+           
+  
+          </ul>
+
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      {/* hero section  */}
+      {/* <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical</h1>
+      </section> */}
+    </>
+  );
+};
+
+export default Navbar;
