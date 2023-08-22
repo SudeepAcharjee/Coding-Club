74040a5800385db9e62015cfa73a69589852f03e
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-08-22 20:18:44 +0530

    Done

--- src/page/home.js
+++ src/page/home.js
@@ -13,14 +13,8 @@ const Home = () => {
       <section className="hero-section">
       <div className="home-container">
       <div className="info-box">
-        <h2>Welcome to our website</h2>
-        <p>This is some introductory text about our website
-          lorem ijdsifj  fhi j ndoihjf  ndioj 9 nhiojf
-          bvgiurh09jr
-          vjoiprjvoprkopvkrf[pvkrk
-          vfrjopkjrokvirp[k rf rkporkorn rrjfjdfopijfoipjdnds i fjfkmdnfkjsdifjdsfjopejrfjdj
-          ffdiovjjopfdjkvokvhnrfkkr[pfi-]ofpjrp[fgjr[ikv;rjvpojrpjfkednriejfoe[pfeinfljop
-          opfgaoprfgopjd;jvkop]]]]].</p>
+        <h2>Welcome to Coding Club </h2>
+        <p>This is an unofficial website handled by the members odf coding club.</p>
         <div className="button-container">
           <button className="centered-button">Button 1</button>
           <button className="centered-button">Button 2</button>
